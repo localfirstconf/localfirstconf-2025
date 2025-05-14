174af5889603fa516430602090ace6b2fb938ae2
Adam Wiggins <a@adamwiggins.com>
2025-05-14 15:04:17 +0200

    community day venue

--- src/app/(app)/schedule/community-day/layout.tsx
+++ src/app/(app)/schedule/community-day/layout.tsx
@@ -20,9 +20,9 @@ export default function Layout({
           <div className="mb-1 uppercase tracking-widest text-orange md:text-lg">Local-First Conf 2025</div>
           <h1 className="font-display text-4xl uppercase leading-none md:text-7xl">Community Day</h1>
           <p className="mb-16 mt-8">
-            The community day is on Monday, May 26. The venue is ??, located at{' '}
-            <a href="#" target="_blank" rel="noreferrer" className="underline">
-              ??
+            The community day is on Monday, May 26. The venue is Delta Campus, located at{' '}
+            <a href="https://maps.app.goo.gl/oio61mjBbdxXNJak8" target="_blank" rel="noreferrer" className="underline">
+              Donaustr. 44, 12043 Berlin, Germany
             </a>
             .
           </p>
